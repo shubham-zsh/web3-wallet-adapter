a2c4281435e390f8a74916149f2aeb069859e941
Shubham Borse <shubhamborse0911@gmail.com>
2025-09-19 14:31:57 +0530

    added airdrop page

---
+++ src/Airdrop.jsx
@@ -0,0 +1,6 @@
+
+export function Airdrop() {
+    return <div>
+        Airdrop
+    </div>
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui;';
+import { Airdrop } from './Airdrop';
 import './App.css'
 
 function App() {
@@ -6,7 +9,16 @@ function App() {
 
   return (
     <>
-      web3
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        < WalletProvider wallets={[]} autoConnect>
+          < WalletModalProvider>
+            <div>
+              hi there <b> hello </b>
+            </div>
+            <Airdrop></Airdrop>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </>
   )
 }
