297254c82f031d6c11d3f18f7dd98b9b5dc33ea4
Shubham Borse <shubhamborse0911@gmail.com>
2025-09-27 15:14:15 +0530

    basic working model

--- src/Airdrop.jsx
+++ src/Airdrop.jsx
@@ -1,6 +1,24 @@
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 export function Airdrop() {
+
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    async function sendAirdropToUser() {
+        let amount = document.getElementById("amount").value;
+        await connection.requestAirdrop(
+            wallet.publicKey,
+            amount * LAMPORTS_PER_SOL)
+        alert("Airdropped " + amount + "Sol to " + wallet.publicKey.toBase58());
+    };
+
+
     return <div>
-        Airdrop
-    </div>
+        <br />
+        <br />
+        <input id="amount" type="text" placeholder="Amount"></ input>
+        <button className="" onClick={sendAirdropToUser}>Send Airdrop</button>
+    </div >
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui;';
+import { useMemo } from 'react'
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react'
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+  WalletDisconnectButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 import { Airdrop } from './Airdrop';
 import './App.css'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = "https://solana-devnet.g.alchemy.com/v2/RWU7SfEV3OADb2pXtI5x0";
 
   return (
     <>
-      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <ConnectionProvider endpoint={endpoint}>
         < WalletProvider wallets={[]} autoConnect>
           < WalletModalProvider>
-            <div>
-              hi there <b> hello </b>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <WalletMultiButton />
+              <WalletDisconnectButton />
             </div>
             <Airdrop></Airdrop>
           </WalletModalProvider>
@@ -23,4 +36,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
